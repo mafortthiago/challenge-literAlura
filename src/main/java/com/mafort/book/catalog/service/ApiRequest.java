40815576754458b9786c95724cfb4c7cac12209c
mafortthiago <thmafort@gmail.com>
2024-06-17 11:09:02 -0300

    feat: Implemented API requester

---
+++ src/main/java/com/mafort/book/catalog/service/ApiRequest.java
@@ -0,0 +1,16 @@
+package com.mafort.book.catalog.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiRequest {
+    public String request(String url) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+}
